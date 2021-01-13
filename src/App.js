85e9483639b1661096db644bd365e0f3af67115c
Ramiro Alvarez <ramiro.al.alvarez@gmail.com>
2021-01-12 23:24:24 -0300

    modifiaciones en atributos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Cotizacion from './components/Cotizacion'
 import Digitador from './components/Digitador';
-//import Resultado from './components/Resultado';
+import Resultado from './components/Resultado';
 
 function App() {
   
+
+  const [valorDolarOficial, setValorDolarOficial] = useState()
+  const [valorDolarBlue, setValorDolarBlue] = useState()
+
   
   
+    const consultarApi = async (e) => {
+
+
+      
+        
+        const api = await fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales');
+        const result = await api.json();
+        let resultado1 = result[0].casa.compra;
+        let resultado2 = result[1].casa.compra;
+        setValorDolarOficial(resultado1);
+        setValorDolarBlue(resultado2);
+        return;
+        
+        
+
+     }
+     
+     consultarApi();
+
+
+
+
+
+   
+      
+      
+
+     
+      
+
   
+
+
+
+
+
+
   
   return (
     
     
     
     <>
+
+    <Cotizacion 
+
+    valorOficial = {valorDolarOficial}
+    valorBlue = {valorDolarBlue}
+
+
+
+    />
+    
+    <Digitador 
+
+    dolarOficialApi = {valorDolarOficial}
+    dolarBlueApi= {valorDolarBlue}
+
+
     
     
-    <Digitador />
     
     
+    />
     
+
+
     
-    
+
+
     </>
 
 
